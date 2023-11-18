6b1f4513874d8b315310cfba6f2a1f033d6dc954
Matt Krozel <mattkrozel@gmail.com>
2023-11-18 13:02:35 -0800

    fix task 0

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,11 +9,11 @@ from sys import argv
 
 if __name__ == '__main__':
     numdone, numtasks = 0, 0
-    userId = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(userId)
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     user_response = requests.get(url)
     url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
-        .format(userId)
+        .format(user_id)
     todo_response = requests.get(url)
     userInfo = json.loads(user_response.text)
     todoInfo = json.loads(todo_response.text)
@@ -21,14 +21,14 @@ if __name__ == '__main__':
     usersname = userInfo['username']
     for task in todoInfo:
         task_dict = {}
-        task_dict['USER_ID'] = userId
+        task_dict['USER_ID'] = user_id
         task_dict['USERNAME'] = usersname
         task_dict['TASK_COMPLETED_STATUS'] = task['completed']
         task_dict['TASK_TITLE'] = task['title']
         tasks.append(task_dict)
 
     names = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    with open('./{}.csv'.format(userId), 'w', encoding='UTF8',
+    with open('./{}.csv'.format(user_id), 'w', encoding='UTF8',
               newline='') as f:
         writer = csv.Dictwriter(f, names=names,
                                 quoting=csv.QUOTE_ALL, quotechar='"')
