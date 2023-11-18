697d8b89237a87cd538501d380761b5082202c74
Matt Krozel <mattkrozel@gmail.com>
2023-11-18 07:11:20 -0800

    first push for task 0 api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,15 +2,26 @@
 '''
 returns to do list info for specific employee
 '''
-import requests
-import sys
+from requests import get
+from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
-    todos = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+    id_user = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id_user)
+    reply = get(url)
+    name = reply.json().get('name')
 
-    completed = [t.get('title') for t in todos if t.get('completed') is True]
-    print('Employee {} is done with tasks({}/{}):'.format(
-        user.get('name'), len(completed), len(todos)))
-    [print('\t {}'.format(c)) for c in completed]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id_user)
+    reply = get(url)
+    jobs = reply.json()
+    done = 0
+    done_jobs = []
+    for job in jobs:
+        if job.get('completed'):
+            done_jobs.append(job)
+            done += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, done, len(jobs)))
+    for job in done_jobs:
+        print('\t {}'.format(job.get('title')))
